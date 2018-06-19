403bc4f75a4ec6035debabee1d93862e285b6467
Daniel Hoornweg <daniel@avantage.nl>
2018-06-19 16:27:57 +0200

    Parses filename. Checks if file is already pressent.

--- src/main.py
+++ src/main.py
@@ -21,20 +21,36 @@ class BaseScraper():
         rq = requests.get('http://horriblesubs.info/lib/getshows.php? \
                             type=show&showid=347')
         soup = BeautifulSoup(rq.text, 'lxml')
-        episode = soup.find("div", {"class":"one-piece-840-1080p"})
+
+        # Get latest epsiode description.
+        episode_string_td = soup.find('tr').find(id).get_text()
+        # Parse the number from the description.
+        episode_number = ([str(s) for s in episode_string_td.split() \
+                            if s.isdigit()][-1])
+        # Use the number to build the class string.
+        episode_class = 'one-piece-%s-1080p' % episode_number
+        # Search for the div containing class string that contains our download.
+        episode = soup.find("div", {"class":"%s" % episode_class})
+        # Get the td thay contains the torrent link.
         episode_1080p = episode.find("td", {"class" : "hs-torrent-link"})
+        # Get the torrent link.
         torrent_link = episode_1080p.a.get('href')
 
-        return torrent_link
+        return torrent_link, episode_number
 
-    def download(self, torrent_link):
+    def download(self, torrent_link, episode_number):
         """
-        Download the torrent file.
+        Download the torrent file, if file already exists, exit.
         """
         r = requests.get(torrent_link, allow_redirects=True)
-        torrent = 'C:/Users/dahoo/Downloads/onepiece840_1080p.torrent'
+        torrent_filepath = 'C:/Users/dahoo/Downloads/onepiece%s.torrent' \
+                            % episode_number
 
-        with open(torrent,
+        if os.path.exists(torrent_filepath):
+            print ('File already exists, exiting now.')
+            exit()
+
+        with open(torrent_filepath,
                     'wb') as f:
             f.write(r.content)
 
@@ -44,7 +60,6 @@ class BaseScraper():
         """
         Start uTorrent to download the epsiode.
         """
-        
         os.system('C:\\Users\\dahoo\\AppData\\Roaming\\uTorrent\\uTorrent.exe \
         /DIRECTORY c:\\Users\\dahoo\\Downloads %s' % torrent)
 
